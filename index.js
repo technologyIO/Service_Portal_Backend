98af7506ede004083a38d7509986f21ddcdd3737
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2025-07-31 18:28:18 +0530

    update

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: true,
+  origin: ['*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
