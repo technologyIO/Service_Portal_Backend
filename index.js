1243594c9742067a9b4a80554acf677dd11dc662
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2025-10-25 16:10:03 +0530

    update

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
